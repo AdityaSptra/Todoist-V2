a5dfef1a3fcf192a59f9200916fbd6de8867efc9
AdityaSptra <aditsaputra0567@gmail.com>
2026-07-21 16:14:30 +0700

    menghubungkan layout page menggunakan fetch

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as nav from "./nav/nav.js"
+import * as page from "./page/page.js"
 
 fetch('./nav/nav.html')
     .then(res => res.text())
@@ -7,4 +8,12 @@ fetch('./nav/nav.html')
         nav.active()
         nav.profilesetting()
         nav.helpdown()
+    })
+
+fetch('./page/page.html')
+    .then(res => res.text())
+    .then(html => {
+        document.querySelector(".page").innerHTML = html
+        page.active()
+        page.menudown()
     })
